c86f76abbcbf2f9d74f1ce4ad6cb1f2d84078c18
deNoor <denoorx@gmail.com>
2023-12-27 22:27:59 +0300

    [Outlaw]: desire 2 RtB buffs with set.

--- Rotations/RogueOutlaw.lua
+++ Rotations/RogueOutlaw.lua
@@ -483,7 +483,7 @@ function rotation:RollTheBones()
         end
     end
 
-    local desiredMin = 2 + (self.AmirSet4p and 1 or 0);
+    local desiredMin = 1 + (self.AmirSet4p and 1 or 0);
 
     local reroll = function()
         if (remains < (self.ShortBursting and 2 or 7)) then
